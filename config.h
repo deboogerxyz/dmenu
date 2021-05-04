c02551b875b27b927b8021389c538ae8e3213661
debooger <debooga@gmail.com>
2021-05-04 22:32:47 +0200

    Changed default line height

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 25;
 static unsigned int min_lineheight = 8;
 
 /*
@@ -24,3 +24,4 @@ static unsigned int min_lineheight = 8;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
